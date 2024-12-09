b1ed417e6623cb71d0f43103274eeb6b5cb0243c
ACryInShame <ciller7765@yahoo.com>
2024-12-09 16:30:11 -0500

    made changes in attempt to solve day 1 part1

--- Avent2024-1.cpp
+++ Avent2024-1.cpp
@@ -7,43 +7,100 @@
 #include "Day2.cpp"
 using namespace std;
 
-void showlist(list<int> L);
+bool  TestValues(list<int> L);
+void TestMessage (string Message);
+bool Testing = true;
+int safeCount = 0;
+
 
 int main()
 {
-	list<int> Reports;
+	list<list<int>> DynamicArray;
 
 	cout << "\n reading file\n--------";
 	string fileInfo = readFile("Puzzles\\AdventDay2.txt");
 
 	cout<< "\n parsing file\n---------";
-	parseReports(fileInfo,Reports);
+	parseReports(fileInfo, DynamicArray);
 
-	showlist(Reports);
+	// Check every set of reports and report back safe (1) or unsafe (0). Keep count of safe reports
+	//loop each report and test
+	for_each
+	(DynamicArray.begin(),DynamicArray.end(), [](const std::list<int> innerList)
+	{
+		// test the set of values
+		bool isSafe = TestValues(innerList);
+		TestMessage("\n is Safe: " + to_string(isSafe) + "\n");
+		if (isSafe)
+			safeCount++;
+	});
 
-	/*
+	cout<< safeCount << " Safe reports\n---------";
 
-	CreateDifferenceList(Cords1,Cords2,DifferenceList);
-
-	sum = std::accumulate(DifferenceList.begin(), DifferenceList.end(), 0);
-	cout << "\n Sum of Differece - Answer: "<<sum << "\n---------";
-
-	cout<< "\n Getting similarity score\n---------------------";
-	CalculateSimilarityScore (Cords1,Cords2,SimilarityScore);
-
-	sum = std::accumulate(SimilarityScore.begin(), SimilarityScore.end(), 0);
-	cout << "\n Sum of Similarity Score - Answer: "<<sum << "\n---------";
-	*/
 	return 0;
 }
 
+void TestMessage (string Message)
+{
+	if (Testing == true)
+		cout << Message;
+}
 
 // function for printing the elements in a list
-void showlist(list<int> L)
+bool  TestValues(list<int> L)
 {
-	list<int>::iterator it;
-	std::for_each (L.begin(),L.end(),[](int num)
+	//set variables
+	int lastNum = -1;
+	int CurrentNum=0;
+	bool isIncreasing = false;
+	bool isDecreasing = false;
+
+	TestMessage("Testing Report: ");
+
+	//loop the list
+	for (int i:L)
 	{
-	cout << num << " " << endl;
-	});
+		CurrentNum = L.front();
+		 TestMessage(to_string(CurrentNum) + " ");
+		//if first number just set and go to next
+		if(lastNum == -1)
+		{
+			lastNum = CurrentNum;
+			L.pop_front();
+			continue;
+		}
+
+		// check if numbers are the same
+		if (CurrentNum == lastNum)
+		{ TestMessage("\nfailed Same check\n"); return 0;}
+
+		//Test if numbers are increasing or decreasing
+		// if both false then hasent been checked yet
+		if ( isIncreasing == false && isDecreasing == false)
+		{
+			if (CurrentNum > lastNum)
+				{isIncreasing = true; TestMessage("^");}
+			else
+				{isDecreasing = true; TestMessage("V");}
+		} else if (isIncreasing == true && CurrentNum < lastNum)
+		{ TestMessage("\nfailed increasing check\n"); return 0;}
+		else if (isDecreasing == true && CurrentNum > lastNum)
+		{ TestMessage("\nFailed Decreasing check\n");return 0;}
+
+		int difference = abs(lastNum - CurrentNum);
+	
+		//test if differnce in numbers are between 1 and 3
+		if (difference < 0 || difference > 3)
+			{
+				TestMessage("\nFailed difference check " + to_string(difference) + "\n");
+				return 0;
+			}
+
+		L.pop_front();
+		lastNum = CurrentNum;
+	}
+	// end of for each
+
+	//if past all tests then set to safe(1)
+	return 1;
 }
